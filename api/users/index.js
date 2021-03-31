0cd6990cc72f7b1d606d6e52322fa31a736625c5
Camilo Acuña <camilo.aku@gmail.com>
2021-03-31 10:22:22 -0300

    update user module

--- api/users/index.js
+++ api/users/index.js
@@ -1,54 +1,24 @@
 // application dependencies
 const express = require("express")
+const db = require("./db")
+const checkRole = require("../../util/check-role")
+const checkValidToken = require("../../util/check-valid-token")
 
 // router definition
 const router = express.Router();
 
-// TODO: migrate below to database
-const users = []
-
 // define endpoints
-router.get('/', (req, res) => {
+router.get('/', checkValidToken(), checkRole("admin"), (req, res) => {
+  // TODO(ca): implements try/catch
+  const users = db.list()
+
   res.json({
     data: users,
   })
 })
-router.post('/', (req, res) => {
-  // get values from body
-  const body = req.body
-  const {name, age} = body
 
-  // check if name body param is defined
-  if (name == null) {
-    return res.status(500).json({
-      error: {
-        message: "invalid name body param",
-      }
-    })
-  }
-
-  // check if age body param is defined
-  if (age == null) {
-    return res.status(500).json({
-      error: {
-        message: "invalid age body param",
-      }
-    })
-  }
-
-  // save user in array
-  users.push({
-    name,
-    age,
-  })
-
-  // endpoint response
-  res.json({
-    data: users,
-  })
-})
 router.get('/:userId/pets/:petId', (req, res) => {
-  const {params} = req
+  const { params } = req
   const { userId, petId } = params
 
   // query db to get by userId
